fd768eb0cec11b6afa511b597a161bf66b005988
prsabahrami <pbahrami@uwaterloo.ca>
2024-06-06 19:55:57 -0400

    Formatting

--- create-rust-app/src/auth/controller.rs
+++ create-rust-app/src/auth/controller.rs
@@ -163,25 +163,17 @@ pub fn get_sessions(
 }
 
 pub fn get_user(access_token: AccessToken) -> Result<ID, (StatusCode, Message)> {
-
     let user_id = match decode::<AccessTokenClaims>(
         &access_token,
         &DecodingKey::from_secret(std::env::var("SECRET_KEY").unwrap().as_ref()),
         &Validation::default(),
     ) {
-        Ok(token)
-            if token
-                .claims
-                .token_type
-                .eq_ignore_ascii_case("access_token") =>
-        {
+        Ok(token) if token.claims.token_type.eq_ignore_ascii_case("access_token") => {
             token.claims.sub
         }
         _ => return Err((401, "Invalid token.")),
     };
-
     Ok(user_id)
-
 }
 
 /// /sessions/{id}
